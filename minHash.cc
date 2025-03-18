759595214b3c9b3ffccc10b76154ceb37a17c923
HalaAlkhatib <hsak2400@gmail.com>
2025-03-18 21:36:04 +0100

    created minHash.cc, tested the algorithm out

---
+++ minHash.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include "xxhash.h"
+using namespace std;
+
+const int T = 10;
+vector<uint32_t> computarMinHash(unordered_set<string> shingles){
+    vector<uint32_t> minhashes(T, UINT32_MAX);
+    for(string shin: shingles){
+        for(int i = 0; i < T; i++){
+            uint32_t hash = XXH32(shin.c_str(), shin.size(), i);
+            minhashes[i] = min(minhashes[i], hash);
+        }
+    }
+    return minhashes;
+}
+
+double similaridades(vector<uint32_t> minhashesA, vector<uint32_t> minhashesB){
+    double count = 0;
+    for(size_t i = 0; i < minhashesA.size(); i++){
+        if(minhashesA[i] == minhashesB[i]){
+            count++;
+        }
+    }
+    cerr << count << endl;
+    return count/T;
+}
+
+
+
+int main(){
+
+    std::unordered_set<std::string> shinglesA = {"algoritmo hashing útil", "hashing útil comparar", "útil comparar documentos"};
+    std::unordered_set<std::string> shinglesB = {"algoritmo hashing útil", "hashing útil comparar", "permite comparar textos"}; 
+
+    std::vector<uint32_t> signatureA = computarMinHash(shinglesA);
+    std::vector<uint32_t> signatureB = computarMinHash(shinglesB);
+
+    double numeros = similaridades(signatureA, signatureB);    
+    cout << numeros << endl;
+    return 0;
+}
