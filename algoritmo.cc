545cb0d3eb8fd985fb206338642bbec711d253d2
HalaAlkhatib <hsak2400@gmail.com>
2025-03-18 21:03:28 +0100

    added xxhash.h and xxhash.c

--- algoritmo.cc
+++ algoritmo.cc
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <unordered_set>
 #include <unordered_map>
-#include <xxhash.h>
-
+#include "xxhash.h"
 using namespace std;
 
+
+int main(){
+    string x = "Hola me llamo Pedro";
+    long long int hash = XXH32(x.c_str(), x.size(), 0);
+    cout << hash << endl;
+    return 0;
+}
